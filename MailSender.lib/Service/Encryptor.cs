a51a863c8aa13210c320538607067b72dbd5ccfe
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-29 21:14:04 +0300

    Подключение БД + NuGet:FontAwesome

---
+++ MailSender.lib/Service/Encryptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.lib.Service
+{
+    public static class Encryptor
+    {
+        public static string Encrypt(string str, int key)
+        {
+            return new string(str.Select(c => (char)(c + key)).ToArray());
+        }
+
+        public static string Decrypt(string str, int key)
+        {
+            return new string(str.Select(c => (char)(c - key)).ToArray());
+        }
+    }
+}
