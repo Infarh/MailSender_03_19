57977bb0d4d7bea24847de64bda546ad6fc4dcf4
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-04-12 20:09:46 +0300

    Пример использования TPL для реализации параллельной обработки данных по средствам P-LINQ и класса Parallel

--- MailSender.Console/Program.cs
+++ MailSender.Console/Program.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Threading;
-
 namespace MailSender.ConsoleTest
 {
     class Program
     {
-        static void Main(string[] args)
+        private static long Factorial(int n)
         {
+            long result = 1;
+            for (var i = 1; i <= n; i++)
+                result *= i;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {  
+            //var factorial_task = new ThreadTask<long>(() => Factorial(24));
+            //factorial_task.Start();
+            //var result_0 = factorial_task.Result;
+            //factorial_task.Wait();
+            //var result = factorial_task.Result;
+
+            //var task = new Task<long>(() => Factorial(24));
+            ////task.RunSynchronously();
+            //task.Start();
+            //var result = task.Result;
+
+            ParallelTest.Test();
+
+
             Console.ReadLine();
         }
     }
