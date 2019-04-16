a72368dffcf993d20fc93637396f22d1eb5288ec
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-04-16 21:10:46 +0300

    Использование OpenXML для генерации документов MS Office

--- MailSender.Console/Program.cs
+++ MailSender.Console/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MailSender.Reports;
 
 namespace MailSender.ConsoleTest
 {
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
+            var report = new Report();
+
+            report.DataField1 = "Value 1";
+            report.DataField2 = "Value 2";
+
+            report.CreatePackage("Report.docx");
+
             Console.ReadLine();
         }
     }
