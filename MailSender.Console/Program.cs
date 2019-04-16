7a6cafb709f35a6b26b063221d9a4a18005829ab
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-04-16 18:02:20 +0300

    Удаление старого кода

--- MailSender.Console/Program.cs
+++ MailSender.Console/Program.cs
@@ -8,34 +8,8 @@ namespace MailSender.ConsoleTest
 {
     class Program
     {
-        public static long Factorial(int n)
-        {
-            long result = 1;
-            for (var i = 1; i <= n; i++)
-                result *= i;
-            return result;
-        }
-
         static void Main(string[] args)
         {  
-            //var factorial_task = new ThreadTask<long>(() => Factorial(24));
-            //factorial_task.Start();
-            //var result_0 = factorial_task.Result;
-            //factorial_task.Wait();
-            //var result = factorial_task.Result;
-
-            //var task = new Task<long>(() => Factorial(24));
-            ////task.RunSynchronously();
-            //task.Start();
-            //var result = task.Result;
-
-            //ParallelTest.Test();
-            //TasksTest.Test();
-
-            AsyncAwaitTest.TestAsync();
-
-
-            Console.WriteLine("Главный поток завершился");
             Console.ReadLine();
         }
     }
--- MailSender.WPF.Test/MainWindow.xaml.cs
+++ MailSender.WPF.Test/MainWindow.xaml.cs
@@ -13,35 +13,5 @@ namespace MailSender.WPF.Test
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
-
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var button = (Button) sender;
-
-            button.IsEnabled = false;
-
-            var progress_reporter = new Progress<int>(p => ProgressBlock.Text = p.ToString());
-
-            var task = Task.Run(() => ComputeOperation(700, progress_reporter));
-
-            var result = await task;
-
-            button.IsEnabled = true;
-
-            ProgressBlock.Text = $"Result = {result}";
-        }
-
-        private long ComputeOperation(int N = 1000, IProgress<int> progress = null)
-        {
-            long result = 0;
-            for (var i = 0; i < N; i++)
-            {
-                Thread.Sleep(10);
-                result += i;
-                progress?.Report(i);
-            }
-
-            return result;
-        }
     }
 }
