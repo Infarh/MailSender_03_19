b18f1aab2ff1e525dfd712c56e251dfe689ae49e
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-04-19 17:00:39 +0300

    Чистка кода

--- MailSender.Console/Program.cs
+++ MailSender.Console/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MailSender.Reports;
 
 namespace MailSender.ConsoleTest
 {
@@ -11,12 +10,6 @@ namespace MailSender.ConsoleTest
     {
         static void Main(string[] args)
         {
-            var report = new Report();
-
-            report.DataField1 = "Value 1";
-            report.DataField2 = "Value 2";
-
-            report.CreatePackage("Report.docx");
 
             Console.ReadLine();
         }
