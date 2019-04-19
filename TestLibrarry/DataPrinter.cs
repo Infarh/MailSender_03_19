fb5f94c0f60ca992e5b5421f4274fabf3a46c1cf
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-04-19 22:53:05 +0300

    Рефлексия

---
+++ TestLibrarry/DataPrinter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace TestLibrarry
+{
+    public class DataPrinter
+    {
+        private string _Data;
+
+        public DataPrinter(string Data) => _Data = Data;
+
+        public void Print(TextWriter writer) => writer.WriteLine($"{DateTime.Now} : {_Data}");
+    }
+}
---
+++ TestLibrarry/InternalDataSummator.cs
@@ -0,0 +1,18 @@
+﻿namespace TestLibrarry
+{
+    internal class InternalDataSummator
+    {
+        private int _Data = 42;
+
+        public int Data
+        {
+            get => _Data;
+            private set => _Data = value;
+        }
+
+        public InternalDataSummator() { }
+        public InternalDataSummator(int Data) => _Data = Data;
+
+        private int Sum(int X) => _Data + X;
+    }
+}
